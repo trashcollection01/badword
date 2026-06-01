d599dc74fb8a71707aaedd95cd5913d41242aff9
jhoonserde <reyhanramadhan9928@gmail.com>
2026-06-01 08:58:27 +0700

    perf: apply UiState in the auth activity

---
+++ app/src/main/java/net/letmethingk/pancode/presentation/ui/activity/auth/register/RegisterUiState.kt
@@ -0,0 +1,15 @@
+package net.letmethingk.pancode.presentation.ui.activity.auth.register
+
+import androidx.compose.foundation.text.input.TextFieldState
+
+class RegisterUiState(
+
+    val usernameInput: TextFieldState = TextFieldState(),
+
+    val emailInput: TextFieldState = TextFieldState(),
+
+    val passwordInput: TextFieldState = TextFieldState(),
+
+    val reenterPasswordInput: TextFieldState = TextFieldState(),
+
+)
